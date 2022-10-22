3d1f9f5a9481816bfda6dea1fafeed7d8b8fd246
Huy Nguyen <touch@nguyenrk.com>
2022-10-22 22:49:58 +0700

    Initial commit

---
+++ CollectionView/CollectionView/MainPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.ObjectModel;
+using Xamarin.Forms;
+
+namespace CollectionView
+{
+    public class Animal
+    {
+        public string name;
+    };
+
+    public partial class MainPage : ContentPage
+    {
+        public ObservableCollection<Animal> collections;
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+            collections = new ObservableCollection<Animal>
+            {
+                new Animal{ name = "this" },
+                new Animal{ name = "that" },
+                new Animal{ name = "something" }
+            };
+
+            collectionview.ItemsSource = collections;
+            collectionview.ItemTemplate = new DataTemplate(() =>
+            {
+                var textCell = new TextCell();
+                textCell.SetBinding(TextCell.TextProperty, "name");
+                textCell.SetBinding(TextCell.DetailProperty, "some details goes here");
+                return textCell;
+            });
+        }
+    }
+}
